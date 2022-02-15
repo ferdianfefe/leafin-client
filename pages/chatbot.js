4c68efc3461945d2bd0185b084fc583104bf4d34
Richardus Ferdian <ferdianditanugraha@gmail.com>
2022-02-15 22:49:11 +0700

    [15-02-2022] Added get message, send message, lazy loading(error)

--- pages/chatbot.js
+++ pages/chatbot.js
@@ -1,53 +1,82 @@
+import {
+  sendMessage,
+  getUserMessages,
+} from "@/components/actions/messageActions";
+import { useState } from "react";
+import useInView from "react-cool-inview";
+import dynamic from "next/dynamic";
+import { useDispatch, useSelector } from "react-redux";
 import Image from "next/image";
 
 export default function chatbot() {
-  const messagesPerDate = [
+  const dispatch = useDispatch();
+  const [messageInput, setMessageInput] = useState("");
+  const [page, setPage] = useState(1);
+
+  const { observe, inView } = useInView({
+    onEnter: ({ unobserve }) => {
+      dispatch(getUserMessages(page, 1)).then((data) => {
+        if (data.length > 0) {
+          setPage(page + 1);
+        }
+      });
+      unobserve();
+    },
+  });
+
+  const messages = [
     {
-      date: "Yesterday",
-      messages: [
-        {
-          message: "Hello, I am your personal chatbot. How can I help you?",
-          user: "bot",
-          createdAt: new Date().getHours() + ":" + new Date().getMinutes(),
-        },
-        {
-          message: "I need to know my plant's water level.",
-          user: "user",
-          createdAt: new Date().getHours() + ":" + new Date().getMinutes(),
-        },
-        {
-          message: "Your plant's water level is at 50%.",
-          user: "bot",
-          createdAt: new Date().getHours() + ":" + new Date().getMinutes(),
-        },
-        {
-          message: "Thank you for your feedback.",
-          user: "user",
-          createdAt: new Date().getHours() + ":" + new Date().getMinutes(),
-        },
-      ],
+      message: "Hello, I am your personal chatbot. How can I help you?",
+      user: "bot",
+      createdAt: "Yesterday",
     },
     {
-      date: "Today",
-      messages: [
-        {
-          message: "Hello, I am your personal chatbot. How can I help you?",
-          user: "bot",
-          createdAt: new Date().getHours() + ":" + new Date().getMinutes(),
-        },
-        {
-          message: "I need to know my plant's PH level",
-          user: "user",
-          createdAt: new Date().getHours() + ":" + new Date().getMinutes(),
-        },
-        {
-          message: "Your plant's PH level is 7.5",
-          user: "bot",
-          createdAt: new Date().getHours() + ":" + new Date().getMinutes(),
-        },
-      ],
+      message: "I need to know my plant's water level.",
+      user: "user",
+      createdAt: "Yesterday",
+    },
+    {
+      message: "Your plant's water level is at 50%.",
+      user: "bot",
+      createdAt: "Yesterday",
+    },
+    {
+      message: "Thank you for your feedback.",
+      user: "user",
+      createdAt: "Yesterday",
+    },
+    {
+      message: "Hello, I am your personal chatbot. How can I help you?",
+      user: "bot",
+      createdAt: "Today",
+    },
+    {
+      message: "I need to know my plant's PH level",
+      user: "user",
+      createdAt: "Today",
+    },
+    {
+      message: "Your plant's PH level is 7.5",
+      user: "bot",
+      createdAt: "Today",
     },
   ];
+
+  const handleMessageChange = (e) => {
+    setMessageInput(e.target.value.trim());
+  };
+
+  const sendMessage = (e) => {
+    console.log(e);
+    e.preventDefault();
+    if (messageInput === "") {
+      return;
+    }
+    dispatch(sendMessage(messageInput)).then((data) => {
+      console.log(data);
+    });
+    setMessageInput("");
+  };
 
   return (
     <div className="container px-5 flex flex-col h-[100vh]">
@@ -78,42 +107,43 @@ export default function chatbot() {
         </div>
       </div>
       <main className="flex-1 overflow-y-auto">
-        {messagesPerDate.map((messagePerDate, index) => (
-          <div key={index} className="flex flex-col">
-            <div className="flex justify-center my-4">
-              <p className="text-[#7B7B7B] text-base">{messagePerDate.date}</p>
-            </div>
-            {messagePerDate.messages.map((message, messageIdx) => (
-              <div key={messageIdx} className="flex my-4">
-                <div className="rounded-full w-12 h-12 relative">
-                  <Image
-                    src="/icon-512x512.png"
-                    objectFit="cover"
-                    layout="fill"
-                    alt="profile picture"
-                    loading="lazy"
-                  ></Image>
-                </div>
-                <div className="flex-1 py-3 px-7 ml-4 rounded-full bg-[#EBEBEB] ">
-                  <p className="text-[#7A7A7A] text-sm">{message.message}</p>
-                </div>
-                <div className="flex-none w-auto ml-2 flex items-center">
-                  <p className="text-sm text-[#ABABAB]">{message.createdAt}</p>
-                </div>
+        {messages.map((message, index) => (
+          <div key={index} className="">
+            {index == 0 ||
+            message.createdAt !== messages[index - 1].createdAt ? (
+              <div className="flex justify-center my-4">
+                <p className="text-[#7B7B7B] text-base">{message.createdAt}</p>
               </div>
-            ))}
+            ) : null}
+            <div ref={observe} className="observer"></div>
+            <div className="flex my-4">
+              <div className="rounded-full w-12 h-12 relative">
+                <Image
+                  src="/icon-512x512.png"
+                  objectFit="cover"
+                  layout="fill"
+                  alt="profile picture"
+                  loading="lazy"
+                ></Image>
+              </div>
+              <div className="flex-1 py-3 px-7 ml-4 rounded-full bg-[#EBEBEB] ">
+                <p className="text-[#7A7A7A] text-sm">{message.message}</p>
+              </div>
+              <div className="flex-none w-auto ml-2 flex items-center">
+                <p className="text-sm text-[#ABABAB]">19:00</p>
+              </div>
+            </div>
           </div>
         ))}
       </main>
-      <div className="flex-none flex py-5">
-        <textarea
+      <form className="flex-none flex py-5">
+        <input
           className="flex-1 placeholder-gray-500 w-full h-[50px] rounded-full px-3 py-3"
-          name=""
-          id=""
-          cols="30"
-          rows="10"
+          type="text"
+          name="message"
           placeholder="Type a message..."
-        ></textarea>
+          onChange={handleMessageChange}
+        ></input>
         <div className="flex-none w-20px h-auto relative ml-5">
           <Image
             src="/assets/attachIcon.svg"
@@ -123,7 +153,11 @@ export default function chatbot() {
             loading="lazy"
           ></Image>
         </div>
-        <div className="flex-none w-20px h-auto relative ml-5">
+        <button
+          onClick={sendMessage}
+          className="flex-none w-20px h-auto relative ml-5"
+          type="submit"
+        >
           <Image
             src="/assets/sendIcon.svg"
             alt="get-started Image"
@@ -131,8 +165,8 @@ export default function chatbot() {
             objectFit="contain"
             loading="lazy"
           ></Image>
-        </div>
-      </div>
+        </button>
+      </form>
     </div>
   );
 }
