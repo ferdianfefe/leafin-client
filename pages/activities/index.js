1f4a11633b52151259a5cd1e93723de12de068d8
Arya Nur <azzakhrufaryannurihsan@gmail.com>
2022-02-26 20:52:37 +0700

    add: marketplace pages

--- pages/activities/index.js
+++ pages/activities/index.js
@@ -2,48 +2,277 @@ import Navbar from '../../components/Navbar';
 import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/css/pagination';
+import Image from 'next/image';
+import Link from 'next/link';
+import { wrapper } from '@/components/store/store';
+import {
+  USER_GET_PROFILE_REQUEST,
+  USER_GET_PROFILE_SUCCESS,
+  USER_GET_USERPLANT_REQUEST,
+  USER_GET_USERPLANT_SUCCESS,
+} from 'constants/userConstants';
+import { getProfile, getServerProfile } from '@/components/actions/userActions';
+import {
+  getServerUserPlant,
+  getUserPlant,
+} from '@/components/actions/userPlantActions';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import Badge from '@/components/Badge';
+
+Activities.getInitialProps = wrapper.getInitialPageProps(
+  ({ getState, dispatch }) =>
+    async ({ req }) => {
+      const { userPlant } = getState();
+      if (userPlant.plants == null && !process.browser) {
+        dispatch({ type: USER_GET_USERPLANT_REQUEST });
+
+        const data = await getServerUserPlant(req);
+
+        dispatch({ type: USER_GET_USERPLANT_SUCCESS, payload: data });
+      } else if (userPlant.plants == null) {
+        dispatch(getUserPlant());
+      } else {
+        console.log('sudah ada data');
+      }
+
+      /* Get profile */
+      const { user } = getState();
+      if (user.user?.data == null && !process.browser) {
+        dispatch({ type: USER_GET_PROFILE_REQUEST });
+
+        const data = await getServerProfile(req);
+
+        dispatch({ type: USER_GET_PROFILE_SUCCESS, payload: data });
+      } else if (user.user?.data == null) {
+        dispatch(getProfile());
+      } else {
+        console.log('sudah ada data');
+      }
+    }
+);
 
 export default function Activities() {
+  let reduxPlant = useSelector((state) => state.userPlant);
+  // const [plant, setPlant] = useState([]);
+
+  // useEffect(() => {
+  //   console.log(reduxPlant?.plants?.data?.plants);
+  //   setPlant(reduxPlant?.plants?.data?.plants || []);
+  // }, [setPlant, reduxPlant?.plants?.data?.plants]);
+
   const test = [
     {
-      text1: 'P',
-      text2: 'HALO',
+      name: 'Water Level',
+      data: [
+        {
+          name: 'Orchidaceae',
+          percentage: 54,
+        },
+        {
+          name: 'Zingiberofficinale',
+          percentage: 29,
+        },
+        {
+          name: 'Jasminum',
+          percentage: 63,
+        },
+        {
+          name: 'Bougainville',
+          percentage: 48,
+        },
+        {
+          name: 'Oryzasativa',
+          percentage: 16,
+        },
+      ],
     },
     {
-      text1: 'P',
-      text2: 'HALO',
+      name: 'Daym Level',
+      data: [
+        {
+          name: 'Orchidaceae',
+          percentage: 54,
+        },
+        {
+          name: 'Zingiberofficinale',
+          percentage: 29,
+        },
+        {
+          name: 'Jasminum',
+          percentage: 63,
+        },
+        {
+          name: 'Bougainville',
+          percentage: 48,
+        },
+        {
+          name: 'Oryzasativa',
+          percentage: 16,
+        },
+        {
+          name: 'Rosa',
+          percentage: 59,
+        },
+      ],
     },
   ];
+
+  const [plant, setPlant] = useState([
+    {
+      _id: 1,
+      name: 'Plant 1',
+      plantType: {
+        pictureFileId: '/assets/leafinLogo.svg',
+      },
+    },
+    {
+      _id: 2,
+      name: 'Plant 2',
+      plantType: {
+        pictureFileId: '/assets/leafinLogo.svg',
+      },
+    },
+    {
+      _id: 3,
+      name: 'Plant 3',
+      plantType: {
+        pictureFileId: '/assets/leafinLogo.svg',
+      },
+    },
+    {
+      _id: 4,
+      name: 'Plant 4',
+      plantType: {
+        pictureFileId: '/assets/leafinLogo.svg',
+      },
+    },
+  ]);
+
+  const todolist = [
+    { svg: '/assets/water.svg', todo: 'Watering Orchidaceae' },
+    { svg: '/assets/water.svg', todo: 'Watering Orchidaceae' },
+
+    { svg: '/assets/water.svg', todo: 'Watering Rosa' },
+  ];
+
   return (
-    <div className="container mx-auto p-5 flex flex-wrap justify-center">
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="font-bold text-2xl px-10 mb-10">Activities</h1>
-        <Swiper
-          observer={true}
-          observeParents={true}
-          pagination={true}
-          modules={[Pagination]}
-        >
-          {test.map((item, index) => {
+    <>
+      <div className="container mx-auto p-5 flex flex-wrap justify-center">
+        {/* <div className="flex flex-col w-full justify-center items-center"> */}
+        <h1 className="font-bold text-2xl text-center px-10 w-full mb-5">
+          Activities
+        </h1>
+        <Swiper className="w-full">
+          {test.map(({ name, data }, i) => {
+            const length = data.length < 5 ? data.length : 5;
+            const width = (1 / length) * 100;
             return (
               <SwiperSlide
-                key={index}
-                className="flex flex-wrap justify-center items-center mb-10"
+                key={i}
+
+                // className="flex flex-wrap justify-center items-center mb-10"
               >
-                <div className="pt-10 mb-5 flex justify-center items-center flex-wrap">
-                  <p className="font-bold text-center w-full text-xl">
-                    {item.text1}
+                <div className="flex justify-center w-full items-center rounded-2xl border-2 mb-10 flex-wrap">
+                  <p className="my-3 ml-5 font-semibold text-start w-full text-md mb-5">
+                    {name}
                   </p>
-                  <p className="mt-5 text-gray-primary text-center">
-                    {item.text2}
-                  </p>
+                  <div className={`flex items-end w-full mx-3`}>
+                    {data.map(({ name, percentage }, i) => {
+                      const height = percentage * 2 + 'px';
+                      const color = percentage > 30 ? '#24A3FF' : '#F2575D';
+                      const textColor = percentage > 30 ? '#000000' : '#F2575D';
+                      if (i < length) {
+                        return (
+                          <div
+                            style={{ width: width + '%' }}
+                            className={`flex flex-col justify-center items-center`}
+                          >
+                            <p style={{ color: textColor }} className="text-sm">
+                              {percentage}%
+                            </p>
+                            <div
+                              style={{
+                                height,
+                                backgroundColor: color,
+                              }}
+                              className={`w-[40px] rounded-t-md`}
+                            ></div>
+                            <p
+                              style={{
+                                wordWrap: 'break-word',
+                              }}
+                              className={`text-xs text-center  w-[50px] bottom-1 absolute items-start`}
+                            >
+                              {name}
+                            </p>
+                          </div>
+                        );
+                      }
+                    })}
+                  </div>
                 </div>
               </SwiperSlide>
             );
           })}
         </Swiper>
+
+        {/* </div> */}
+
+        <div className="font-bold mt-10 w-full">
+          <label className="text-left">All Plants</label>
+          <div className="flex mt-3 justify-between flex-wrap gap-y-3">
+            <Swiper
+              slidesPerView={3}
+              spaceBetween={5}
+              slidesPerGroup={3}
+              loopFillGroupWithBlank={true}
+            >
+              {plant.map(({ name, _id, plantType }, i) => {
+                return (
+                  <SwiperSlide key={i}>
+                    <Link href={'activities/' + _id}>
+                      <a className="bg-[#DCFFE3] pt-2 pb-9 px-3 rounded-full flex flex-col items-center justify-center">
+                        <div className="w-20 h-20 rounded-full relative overflow-hidden">
+                          <Image
+                            src={plantType.pictureFileId}
+                            objectFit="cover"
+                            layout="fill"
+                            alt="profile picture"
+                            loading="lazy"
+                          ></Image>
+                        </div>
+                        <p className="mt-5">{name}</p>
+                      </a>
+                    </Link>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
+        </div>
+
+        <div className="font-bold mt-10 w-full mb-14">
+          <label className="text-left">Todo List</label>
+          <div className="flex flex-wrap gap-2 justify-center w-full items-center mt-5">
+            {todolist.map(({ svg, todo }, i) => {
+              return (
+                <div
+                  key={i}
+                  className="flex items-center w-full border-2 px-2 h-12 rounded-lg"
+                >
+                  <div className="w-6 h-6 mr-2 relative">
+                    <Image src={svg} layout="fill" alt="ph" />
+                  </div>
+                  <span className="ml-1 font-semibold">{todo}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
       <Navbar active={'activities'}></Navbar>
-    </div>
+    </>
   );
 }
---
+++ pages/marketplace/index.js
@@ -0,0 +1,139 @@
+import Button from '@/components/Button';
+import FilterItem from '@/components/FilterItem';
+import Navbar from '@/components/Navbar';
+import { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Marketplace() {
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  const filters = ['Fertilizer', 'Tools', 'Seeds', 'Leafin'];
+
+  const selectFilter = (filter) => {
+    if (selectedFilters.includes(filter)) {
+      setSelectedFilters(selectedFilters.filter((item) => item !== filter));
+    } else {
+      setSelectedFilters([...selectedFilters, filter]);
+    }
+  };
+
+  const barang = [
+    {
+      stars: 4,
+      name: 'Pupuk Nafos',
+      price: '12.69',
+      image: '/assets/Pupuk-Nafos.png',
+    },
+    {
+      stars: 4,
+      name: 'Pupuk ZA',
+      price: '12.69',
+      image: '/assets/Pupuk-Za.png',
+    },
+    {
+      stars: 4,
+      name: 'Pupuk Kaltim',
+      price: '12.69',
+      image: '/assets/Pupuk-Kaltim.png',
+    },
+    {
+      stars: 4,
+      name: 'Pupuk Dinosaurus',
+      price: '12.69',
+      image: '/assets/Pupuk-Dinosaurus.png',
+    },
+    {
+      stars: 4,
+      name: 'Pupuk Petro',
+      price: '12.69',
+      image: '/assets/Pupuk-Petro.png',
+    },
+    {
+      stars: 3,
+      name: 'Pupuk Biotara',
+      price: '12.69',
+      image: '/assets/Pupuk-Biotara.png',
+    },
+  ];
+
+  const funcStars = (stars) => {
+    let result = [];
+    for (let i = 0; i < stars; i++) {
+      result.push(
+        <div className="w-[9px] h-[9px] relative">
+          <Image src="/assets/stars.svg" alt="stars" layout="fill" />
+        </div>
+      );
+    }
+    return result;
+  };
+  return (
+    <>
+      <div className="container mx-auto p-5 flex flex-wrap justify-center">
+        <div className="w-full">
+          <h1 className="font-bold text-[20px] w-2/3 text-left">
+            We provide everything you need
+          </h1>
+        </div>
+
+        <div className="flex flex-wrap items-center w-full justify-start">
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={5}
+            slidesPerGroup={3}
+            loopFillGroupWithBlank={true}
+            className="mt-2"
+          >
+            {filters.map((filter, i) => {
+              return (
+                <SwiperSlide key={i}>
+                  <Button
+                    onClick={() => {
+                      selectFilter(filter);
+                    }}
+                  >
+                    <FilterItem
+                      text={filter}
+                      active={selectedFilters.includes(filter)}
+                    />
+                  </Button>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
+
+        <div className="flex flex-wrap items-center w-full justify-between mb-20">
+          {barang.map(({ stars, name, price, image }, i) => {
+            return (
+              <Link key={i} href={'marketplace/' + name}>
+                <a className="h-[269px] rounded-xl w-[48%] mb-3">
+                  <div className="relative h-[182px] overflow-hidden rounded-t-xl">
+                    <Image
+                      src={image}
+                      alt={name}
+                      loading="lazy"
+                      layout="fill"
+                    />
+                  </div>
+                  <div className="border-x-2 border-b-2 rounded-b-xl flex flex-col px-3">
+                    <div className="pt-2 pb-1 flex gap-[2px]">
+                      {funcStars(stars)}
+                    </div>
+                    <div className="font-semibold mb-2">{name}</div>
+                    <div className="font-bold mb-1">{price}</div>
+                  </div>
+                </a>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+
+      <Navbar active={'marketplace'} />
+    </>
+  );
+}
